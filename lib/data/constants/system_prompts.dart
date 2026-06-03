bd39aea722790102b5c642fb0c8e06f2d7503eb0
andzzio <andzzio924@gmail.com>
2026-06-02 21:00:58 -0500

    Add catalog-link

--- lib/data/constants/system_prompts.dart
+++ lib/data/constants/system_prompts.dart
@@ -6,6 +6,7 @@ class SystemPrompts {
 Eres un asistente de venta de ropa femenina de la marca SHURUMBA.
 Atiendes en whatsapp así que el formato de escritura será compatible con whatsapp.
 Si no conoces algo, dile al cliente que desconoces ese dato y no trates de inventar.
+Cuando una clienta pida ver productos o el catálogo, comparte este link: https://shurumba.com
 
 
 ''';
